4e1146ddc9b6fec6dd3cdf4d9174c6f8003564de
er5ctyvgbhujnikmolp <mr.windows3001@gmail.com>
2025-03-08 10:42:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = (req, res) => {
-  // Перенаправление всех запросов на api.zelenka.guru
-  const proxy = createProxyMiddleware({
-    target: 'https://api.zelenka.guru', // Целевой домен
-    changeOrigin: true, // Изменяет origin запроса на целевой домен
-    onProxyReq: (proxyReq, req) => {
-      // Обновляем заголовки, если нужно
-      proxyReq.setHeader('host', 'api.zelenka.guru');
-    },
-  });
+const proxy = createProxyMiddleware({
+  target: 'https://api.zelenka.guru',
+  changeOrigin: true,
+  onProxyReq: (proxyReq) => {
+    proxyReq.setHeader('host', 'api.zelenka.guru');
+  },
+});
 
-  proxy(req, res, (err) => {
-    if (err) {
-      res.status(500).json({ error: 'Proxy Error', details: err.message });
-    }
+module.exports = (req, res) => {
+  return new Promise((resolve, reject) => {
+    proxy(req, res, (result) => {
+      if (result instanceof Error) {
+        res.status(500).json({ error: 'Proxy Error', details: result.message });
+        reject(result);
+      } else {
+        resolve();
+      }
+    });
   });
 };
